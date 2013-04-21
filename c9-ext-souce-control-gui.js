87abc9978f25ea0aea6c7bc7848ab21e9c9df19c
petemill <petemill@live.com>
2013-04-21 09:25:20 -0400

    Adds a Mercurial Commit menu command which displays a dialog with a checkbox for each modified, new and deleted file.

--- c9-ext-souce-control-gui.js
+++ c9-ext-souce-control-gui.js
@@ -1,251 +1,178 @@
-/**
- * Extension Template for Cloud9 IDE
- * 
- * Inserts a context menu item under the "Edit" menu, which, when
- * clicked, displays a simple window with a "Close" button.
- * 
- * This file is stripped of comments in order to provide a quick template for 
- * future extensions. Please reference our documentation for a list of what's
- * going on.
- *
- * @copyright 2012, Ajax.org B.V.
- * @license GPLv3 <http://www.gnu.org/licenses/gpl.txt>
- */
- 
+
+(function() {
+    
+var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+  
 define(function(require, exports, module) {
-
-var ext     = require("core/ext");
-var ide     = require("core/ide");
-var menus   = require("ext/menus/menus");
-var editors = require("ext/editors/editors");
-var BlameJS = require("ext/gitblame/blamejs");
-var util    = require("core/util");
-var commands = require("ext/commands/commands");
-
-//var markup = require("text!extension_template.xml");
-
-module.exports = ext.register("extension_template", {
-    name     : "Extension Template",
-    dev      : "Ajax.org",
-    alone    : true,
-    deps     : [],
-    type     : ext.GENERAL,
-
-    nodes : [],
-
-    init : function(){
-        var _self = this;
-        //this.winExtensionTemplate = winExtensionTemplate;
-        commands.addCommand({
-            name: "sayhello",
-            hint: "I'll say something",
-            msg: "this is where msg is",
-            bindKey: {mac: "Shift-1", win: "Ctrl-1"},
-            isAvailable : function() {
-                return true;    
-            },
-            exec: function(){_self.requestStatus();}
-            // function() {
-            //     console.log('hi');
-            //           var data = {
-            //                 command : 'gittools',
-            //                 subcommand : "log"
-            //             };
-            //             console.log('dispatchevent:');
-            //     ide.dispatchEvent("track_action", {type: "gittools", cmd: 'gittools', subcommand: data.subcommand});
-            //     console.log('dispatchevent:done');
-            //     if (ext.execCommand('gittools', data) !== false) {
-            //         if (ide.dispatchEvent("consolecommand." + 'gittools', {
-            //           data: data
-            //         }) !== false) {
-            //             if (!ide.onLine) {
-            //                 util.alert(
-            //                     "Currently Offline",
-            //                     "Currently Offline",
-            //                     "This operation could not be completed because you are offline."
-            //                 );
-            //                 console.log('alert');
-            //             }
-            //             else {
-            //                 ide.send(data);
-            //                console.log('send');
-            //             }
-            //         }
-            //         else
-            //         {
-            //             console.log('dispatchevent was false');
-            //         }
-            //     }
-            //     else
-            //     {
-            //         console.log('execcommand was false');
-            //     }
-            // }
-        });
       
+    var editors, ext, ide, markup, util, menus;
+    ide = require('core/ide');
+    ext = require('core/ext');
+    util = require('core/util');
+    editors = require('ext/editors/editors');
+    markup = require('./markup-ams.xml.js');
+    menus = require("ext/menus/menus");
+    
+    return module.exports = ext.register('ext/scratchpad/scratchpad', {
+        name     : "Source Control GUI",
+        dev      : "Wishfulcode",
+        type: ext.GENERAL,
+        alone: true,
+        markup: markup,
+        hotitems: {},
+        nodes: [],
+
+        hook: function() {
+            var mnuView = menus.addItemByPath("View");
+            this.nodes.push(mnuView.appendChild(new apf.item({
+              caption: 'Mercurial Commit',
+              onclick: __bind(function() {
+                this.beginMercurialCommit();
+              }, this)
+            })));
+        },
         
+        beginMercurialCommit: function() {
+            ext.initExtension(this);
+            //show the window
+            this.scratchpadWindow.show();
+            //begin the process by checking which files have been modified
+            this.runCommand('hgstatus',["hg", "status"], '.');
+            //display waiting status
+            this.tempStatusLine = new apf.checkbox({
+                id   : "chk1",
+                label : "Waiting for data..."
+            });
+            this.uiStatusLines.appendChild(this.tempStatusLine);
+        },
         
+        init: function() {
+            
+            //define UI item references
+            this.uiStatusLines = statusLines;
+            this.scratchpadAdd = scratchpadAdd;
+            this.scratchpadClose = scratchpadClose;
+            this.scratchpadWindow = scratchpadWindow;
+            
+            this.scratchpadClose.addEventListener('click', __bind(function() {
+              return this.scratchpadWindow.close();
+            }, this));
+            
+            
+        },
         
-        this.nodes.push(
-            menus.addItemByPath("Edit/Extension Template", new apf.item({
-                command : "sayhello"
-            }), 5400)
-        ); 
-
-       /* Just a plain menu...
-        this.nodes.push(
-            menus.addItemByPath("Edit/Extension Template", new apf.item({
-                onclick : function(){
-                    _self.winExtensionTemplate.show();
-                }
-            }), 5400)
-        ); */
-    },
-    requestStatus : function() {
-        console.log('status begin')
-        var cmd = "gittools";
-        //var page = tabEditors.getPage();
-        // if (!page)
-        //     return;
-//         var path = page.$model.data.getAttribute("path");
-// 
-//         var lastSlash = path.lastIndexOf("/");
-//         var fileName = path.substr(lastSlash + 1);
-//         var dirName = path.substring(ide.davPrefix.length + 1, lastSlash);
-//         if (dirName == "/")
-           var dirName = ide.workspaceDir;
-        // else
-        //     dirName = ide.workspaceDir + "/" + dirName;
-
-        var data = {
-            command: "git",
-            argv: ["hg", "status"],
-            extra: {type: "gitblame",scccmd: 'hgstatus', path:".", original_line: ""},
-            requireshandling: !commands.commands.git,
-            cwd: dirName // needed for nested repositories
-        };
-        // @todo should we change server side plugin to not require this?
-        data.line = data.argv.join(" ");
-
-        if (!this.$onMessage) {
-            this.$onMessage = this.onMessage.bind(this);
-            ide.addEventListener("socketMessage", this.$onMessage);
-            this.blamejs = {};
-        }
-        //this.blamejs[path] = new BlameJS();
-
-        var status = "Loading...";
-        ide.dispatchEvent("track_action", {type: "blame", cmd: cmd});
-        if (ext.execCommand(cmd, data) !== false) {
-            if (ide.dispatchEvent("consolecommand." + cmd, {data: data}) !== false) {
-                if (!ide.onLine) {
-                    status = "This operation could not be completed because you are offline.";
-                }
-                else {
-                    ide.send(data);
+        enable: function() {
+            this.nodes.each(function(item) {
+              item.enable();
+            });
+        },
+        
+        disable: function() {
+            this.nodes.each(function(item) {
+              item.disable();
+            });
+        },
+        
+        destroy: function() {
+            this.nodes.each(function(item) {
+              item.destroy(true, true);
+            });
+            this.nodes = [];
+            this.scratchpadClose.removeEventListener('click');
+            this.scratchpadAdd.removeEventListener('click');
+            this.scratchpadTabs.destroy(true, true);
+            this.scratchpadAdd.destroy(true, true);
+            this.scratchpadClose.destroy(true, true);
+            this.scratchpadWindow.destroy(true, true);
+        },
+        
+        onHgStatus : function(message) {
+            console.log('on hg status');
+            
+            //parse hg status results
+            var statusRaw = message;
+            var statusLines = statusRaw.split('\n');
+            console.log('split lines', statusLines);
+            //display results
+            for (var statusLinesIt in statusLines) {
+                var statusLine = statusLines[statusLinesIt];
+                if (statusLine !== "" && typeof statusLine === "string") {
+                     this.uiStatusLines.appendChild(new apf.checkbox({
+                        label : statusLine
+                    }));
                 }
             }
-        }
-       // this.displayGutter([{text: status, title: ""}], path);
-    },
-    onMessage: function(e) {
-        var message = e.message;
-        //console.log('onMessage');
-        //console.log('type and subtype:' + message.type + ' ' + message.subtype);
-        //console.log('extra and extra type:' + message.extra + message.extra.type);
-        if (!message.extra || message.extra.type != "gitblame")
-            return false;
-        // if (!this.blamejs[message.extra.path])
-        //     return;
+            
+            //temp: show the raw results
+            this.tempStatusLine.setAttribute('label',statusRaw);
+
+        },
         
-        var type = message.type.substr(-5);
-        if (type == "-exit") {
-            // message.code && util.alert(
-            //     "Error", "There was an error returned from the server:",
-            //     message.data
-            // );
-            // delete this.blamejs[path];
-            return;
-        }
-        // Is the body coming in piecemeal? Process after this message
-        if (type != "-data" || !message.data) {
-            return;
-        }
+        onMessage: function(e) {
+            var message = e.message;
+            
+            if (!message.extra || message.extra.type != "gitblame")
+                return false;
+            
+            var type = message.type.substr(-5);
+            if (type == "-exit") {
+                return;
+            }
+            // Is the body coming in piecemeal? Process after this message
+            if (type != "-data" || !message.data) {
+                return;
+            }
+            var commandType = message.extra.scccmd;
+    
+            //real stuff here, parse output
+            console.log(commandType + " message received back from server:");
+            console.log(message.data);
+            
+            switch(commandType) {
+                case "hgstatus":
+                    this.onHgStatus(message.data);
+            }
+        },
         
-        var commandType = message.extra.scccmd;
+        runCommand : function(scccmd, argv, path) {
         
-        //real stuff here, parse output
-        console.log(commandType + " message received back from server:");
-        console.log(message.data);
-        
-        switch(commandType) {
-            case "hgstatus":
-                this.onHgStatus(message.data);
-        }
-//         var path = message.extra.path;
-//         var blamejs = this.blamejs[path];
-// 
-//         if (!blamejs.parseBlame(message.data)) {
-//             util.alert(
-//                 "Problem Parsing", "Problem Parsing",
-//                 "There was a problem parsing the blame output. Blame us, blame the file, but don't blame blame.\nBlame."
-//             );
-//             return false;
-//         }
-// 
-//         // Now formulate the output
-//         this.formulateOutput(blamejs, path);
-    },
-
-    onHgStatus : function(message) {
-        console.log('on hg status');
-        // this.gitLogs[message.body.file].revisions[message.body.hash] =
-        //     message.body.out;
-        // editors.currentEditor.amlEditor.getSession().setValue(message.body.out);
-        // editors.currentEditor.amlEditor.$editor.setReadOnly(true);
-    },
-
-    onGitBlameMessage: function(message) {
-        if (!this.blamejs.parseBlame(message.body.out)) {
-            util.alert(
-                "Problem Parsing",
-                "Problem Parsing",
-                "There was a problem parsing the blame output."
-            );
-
-            return false;
-        }
-
-        this.outputGitBlame(this.blamejs.getCommitData(), this.blamejs.getLineData());
-    },
-
-    hook : function(){
-        var _self = this;
-        ext.initExtension(this);
-    },
-
-    enable : function(){
-        this.nodes.each(function(item){
-            item.enable();
-        });
-    },
-
-    disable : function(){
-        this.nodes.each(function(item){
-            item.disable();
-        });
-    },
-
-    destroy : function(){
-        this.nodes.each(function(item){
-            item.destroy(true, true);
-        });
-        this.nodes = [];
-    },
-
-     closeExtensionTemplateWindow : function(){
-       // this.winExtensionTemplate.hide();
-     }
+            console.log('runCommand');
+            console.log('argb',argv,'sccmd',scccmd,'path',path);
+            var cmd = "gittools";
+            
+            var dirName = ide.workspaceDir;
+            
+            var data = {
+                command: "git",
+                argv: argv,
+                extra: {type: "gitblame",scccmd: scccmd, path:path, original_line: ""},
+                requireshandling: true,
+                cwd: dirName 
+            };
+            // server side plugin requires this
+            data.line = data.argv.join(" ");
+            
+            if (!this.$onMessage) {
+                this.$onMessage = this.onMessage.bind(this);
+                ide.addEventListener("socketMessage", this.$onMessage);
+                this.blamejs = {};
+            }
+            
+            var status = "Loading...";
+            ide.dispatchEvent("track_action", {type: "blame", cmd: cmd});
+            if (ext.execCommand(cmd, data) !== false) {
+                if (ide.dispatchEvent("consolecommand." + cmd, {data: data}) !== false) {
+                    if (!ide.onLine) {
+                        status = "This operation could not be completed because you are offline.";
+                    }
+                    else {
+                        ide.send(data);
+                    }
+                }
+            }
+            
+        },
+    });
 });
+}).call(this);
 
-});
---
+++ markup-ams.xml.js
@@ -0,0 +1,23 @@
+// Wrapped in JavaScript, to avoid cross-origin restrictions, created using wrap-in-js.sh
+define(function() {
+return '<a:application xmlns:a="http://ajax.org/2005/aml">\n' +
+'    <a:window\n' +
+'        id = "scratchpadWindow"\n' +
+'        title = "Scratchpad"\n' +
+'        center = "true"\n' +
+'        modal = "false"\n' +
+'        buttons = "close"\n' +
+'        kbclose = "true"\n' +
+'        width = "800"\n' +
+'        height = "500">\n' +
+'        \n' +
+'        <a:vbox heigh="400" id="statusLines">\n' +
+'            \n' +
+'        </a:vbox>\n' +
+'        <a:divider />\n' +
+'        <a:hbox pack="start" padding="5" edge="10 10 5 10">\n' +
+'            <a:button id="scratchpadAdd">Add New Pad</a:button>\n' +
+'            <a:button id="scratchpadClose">Close</a:button>\n' +
+'        </a:hbox>\n' +
+'    </a:window>\n' +
+'</a:application>\n' +'';});
